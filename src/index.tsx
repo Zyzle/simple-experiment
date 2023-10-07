e37d9e813e3571ff3e1c77c98e65fcef9b2c5fe1
Colin McCulloch <colin.mcculloch@whocanfixmycar.com>
2023-10-07 16:54:29 +0100

    cleaning up old analytics before deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { Analytics, AnalyticsInstance } from "analytics";
 import { createRoot } from "react-dom/client";
-
-import mixpanelPlugin from "@analytics/mixpanel";
+import amplitudePlugin from "@analytics/amplitude";
 import { GrowthBook, GrowthBookProvider } from "@growthbook/growthbook-react";
 import { AnalyticsProvider } from "use-analytics";
 
@@ -13,31 +12,47 @@ const growthbook = new GrowthBook({
   apiHost: "https://cdn.growthbook.io",
   clientKey: "sdk-moFjHQypBadgSE5t",
   enableDevMode: true,
-  trackingCallback: (experiment, result) => {
-    analytics.track("$experiment_started", {
-      "Experiment name": experiment.key,
-      "Variant name": result.variationId,
-      $source: "growthbook",
-    });
-  },
+  // trackingCallback: (experiment, result) => {
+  //   analytics.track("$experiment_started", {
+  //     "Experiment name": experiment.key,
+  //     "Variant name": result.variationId,
+  //     $source: "growthbook",
+  //   });
+  // },
 });
 
 analytics = Analytics({
   app: "experiment-testing",
   plugins: [
-    mixpanelPlugin({
-      token: "b941888e0ba7beb2cc691a906a8fd6b3",
-      options: {
-        loaded: (mx) => {
-          growthbook.setAttributes({
-            ...growthbook.getAttributes(),
-            id: mx.get_distinct_id(),
-          });
-        },
-      },
-    }),
+    // amplitudePlugin({
+    //   apiKey: "23f53bd94cd4b9bfcca056067a7fdaea",
+    //   // See options at https://bit.ly/3dRdZnE
+    //   options: {
+    //     trackingOptions: {
+    //       ip_address: false,
+    //     },
+    //   },
+    // }),
+    // mixpanelPlugin({
+    //   token: "b941888e0ba7beb2cc691a906a8fd6b3",
+    //   options: {
+    //     loaded: (mx) => {
+    //       growthbook.setAttributes({
+    //         ...growthbook.getAttributes(),
+    //         id: mx.get_distinct_id(),
+    //       });
+    //     },
+    //   },
+    // }),
   ],
   debug: true,
+});
+
+analytics.ready(() => {
+  // growthbook.setAttributes({
+  //   ...growthbook.getAttributes(),
+  //   id: analytics.user().anonymousId,
+  // });
 });
 
 growthbook.loadFeatures({ autoRefresh: true });
